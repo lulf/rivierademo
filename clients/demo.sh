715db0656243f063172d0cd9b4efc6eea25afc60
Ulf Lilleengen <lulf@redhat.com>
2017-04-28 20:21:48 +0200

    Fix demo

--- clients/demo.sh
+++ clients/demo.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-WAIT_TIME=${1:-600}
 /oc_freceive anycast &
-/oc_freceive anycast &
-
 /oc_freceive queue1 &
 /oc_freceive queue1 &
-/oc_freceive queue1 &
-
-/oc_rsend anycast &
-/oc_rsend anycast &
-/oc_rsend queue1 &
-
-sleep $WAIT_TIME
+sleep 20
+/oc_rsend anycast $ADDRESS &
+/oc_rsend queue1 $ADDRESS
